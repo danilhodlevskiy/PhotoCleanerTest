7f08f42fb3a5599b08391ff4c5172c931d17126e
danilhodlevskiy <danilhodlevskiy@gmail.com>
2026-01-22 16:34:04 +0200

    Initial Commit

---
+++ PhotoCleanerTest/PhotoCleanerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanerTestApp.swift
+//  PhotoCleanerTest
+//
+//  Created by danilka on 22.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCleanerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
